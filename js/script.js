94cd014ed182689ba9ff5305cbb6ecc9088025c3
Mirko <mirkosbaglia2001@gmail.com>
2021-03-17 18:16:00 +0100

    Esercizio terminato

---
+++ js/script.js
@@ -0,0 +1,61 @@
+// Un alert() espone 5 numeri generati casualmente.
+// Da li parte un timer di 30 secondi.
+// Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
+//Function alerts()
+function alerts(){
+  numMax = 100;
+  numMin = 1;
+  randomArr = [];
+  for (var i = 0; i < 5; i++) {
+    var numRandom = (Math.floor(Math.random() * numMax + numMin));
+    randomArr.push(numRandom);
+  }
+  console.log('array:', randomArr);
+  alert(randomArr);
+}
+alerts();
+
+//Function timer 30 secondi
+var time;
+var clock;
+function tick(){
+  document.getElementById('title').innerHTML = time++;
+  if(time > 30){
+    clearInterval(clock);
+    prompts();
+  }
+  // console.log('tick');
+}
+
+function interval(){
+  time = 1;           //function
+  clock = setInterval(   tick,         1000);
+}
+interval();
+
+
+// prompt
+function prompts(){
+  count = 0;
+  var promptArr = [];
+  for (var i = 0; i < 5; i++) {
+    var promptNum = parseInt(prompt('Inserisci i cinque numeri che hai visto precendentemente, uno alla volta: '));
+    if (randomArr.includes(promptNum)) {
+      promptArr.push(promptNum);
+      count++;
+    }
+  }
+  var howMany = document.getElementById('quantinumeri');
+  howMany.innerHTML = 'Su 5 numeri mostrati, hai indovinato ' + count + ' numeri';
+  if(promptArr != ''){
+    var numGuessed = document.getElementById('numerindovinati');
+    numGuessed.innerHTML = 'I numeri indovinati sono i seguenti: ' + promptArr;
+  }
+
+
+  // console.log('I numeri indovinati sono i seguenti: ', promptArr, 'Numeri indovinati: ',count );
+
+}
